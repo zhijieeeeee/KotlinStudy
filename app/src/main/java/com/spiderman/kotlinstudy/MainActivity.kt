3281d89bf038a8b6fb3be36b853676e1adf25187
tzj <3003745327@qq.com>
2019-02-25 17:40:38 +0800

    update

--- app/src/main/java/com/spiderman/kotlinstudy/MainActivity.kt
+++ app/src/main/java/com/spiderman/kotlinstudy/MainActivity.kt
@@ -7,6 +7,7 @@ import android.util.Log
 import android.view.View
 
 import kotlinx.android.synthetic.main.activity_main.*
+import java.io.File
 
 class MainActivity : AppCompatActivity() {
 
@@ -41,11 +42,12 @@ class MainActivity : AppCompatActivity() {
         z = 3// 明确赋值
 
         text = "latter"
+        Log.i("MyLog", "$text.length=${text.length}")
         p++
 
         Log.i("MyLog", s)
         a = 2
-        val s2 = "${s.replace("is", "was")} but now is $a"
+        val s2 = "${s.replace("is", "was")} but now is ${a++}"
         Log.i("MyLog", s2)
 
         printList()
@@ -53,6 +55,9 @@ class MainActivity : AppCompatActivity() {
 
         Log.i("MyLog", describe(1))
         Log.i("MyLog", describe("S"))
+        Log.i("MyLog", "return ${describe(1.002f)}")
+
+        returnWhen("Purple")
 
         rangeTest(1)
         rangeTest(10)
@@ -60,10 +65,41 @@ class MainActivity : AppCompatActivity() {
         flow()
 
         mapTest()
+
+        var sentence = "Tang Zhi Jie"
+
+        Log.i("MyLog", "${sentence.deleteSpace()}")
+
+        testNull()
+
+        testArray()
+
+        //if
+        val max = if (z > y) z else y
+        //if的分支可以是代码块，最后的表达式作为该块的值：
+        val max1 =
+            if (z > y) {
+                Log.i("MyLog", "z>y")
+                z
+            } else {
+                Log.i("MyLog", "z<y")
+                y
+            }
+
+        //可以用when 替代 if else if else
+        var w = 4
+        when {
+            w > 4 -> Log.i("MyLog", "w > 4")
+            w < 4 -> Log.i("MyLog", " w < 4")
+            else -> Log.i("MyLog", "w = 4")
+        }
+
+        testBreak()
+        testReturn()
     }
 
     //返回int
-    fun sum(a: Int, b: Int): Int {
+    fun sum(a: Int = 1, b: Int): Int {
         return a + b
     }
 
@@ -81,7 +117,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     //当某个变量的值可以为 null 的时候，必须在声明处的类型后添加 ? 来标识该引用可为空。
-    fun parseInt(str: String): Int? {
+    fun parseInt(str: String = ""): Int? {
         return str.toIntOrNull()
     }
 
@@ -106,16 +142,38 @@ class MainActivity : AppCompatActivity() {
         for (index in list.indices) {
             Log.i("MyLog", "item[$index] is ${list[index]}")
         }
+        for ((index, value) in list.withIndex()) {
+            Log.i("MyLog", "$index is $value")
+        }
     }
 
-    fun describe(obj: Any): String =
+    fun describe(obj: Any): String? =
         when (obj) {
             1 -> "Int"
             "Hello" -> "String"
             is Long -> "Long"
+            is String -> "String"
             !is String -> "Not a String"
-            else -> "Unknown"
+            else -> null
         }
+
+    fun returnWhen(color: String): Int {
+        return when (color) {
+            "Red", "Black", "White" -> 0
+            "Yellow" -> 1
+            "Blue" -> 2
+            else -> 3
+//            else -> throw IllegalArgumentException("Invalid color param value")
+        }
+    }
+
+    fun returnWhen2(color: String): Int = when (color) {
+        "Red" -> 0
+        "Yellow" -> 1
+        "Blue" -> 2
+        else -> throw IllegalArgumentException("Invalid color param value")
+    }
+
 
     fun rangeTest(x: Int) {
         if (x in 1..5) {
@@ -130,7 +188,7 @@ class MainActivity : AppCompatActivity() {
 
         //10到5
         for (x in 10 downTo 5) {
-            Log.i("MyLog", "x=${x}")
+            Log.i("MyLog", "x=$x")
         }
     }
 
@@ -149,5 +207,65 @@ class MainActivity : AppCompatActivity() {
             Log.i("MyLog", "$key:$value")
         }
         Log.i("MyLog", "map[a]=${map["a"]}")
+    }
+
+    //扩展方法
+    private fun String.deleteSpace(): String {
+        return this.replace(" ", "")
+    }
+
+    fun testNull() {
+        var files = File("ss").listFiles()
+        //if not null 缩写
+        Log.i("MyLog", "${files?.size}")
+        //if not null else缩写
+        Log.i("MyLog", "${files?.size ?: "empty"}")
+        //if null    ?:
+        Log.i("MyLog", "${files ?: "空的"}")
+    }
+
+    fun testArray() {
+        val a = arrayOf(1, 2, 3, 4)
+        //循环方式1
+        for (item in a) {
+            Log.i("MyLog", "$item")
+        }
+        //循环方式2
+        a.forEach { Log.i("MyLog", "$it") }
+
+        val a2 = Array(5, { i -> (i * i).toString() })
+        a2.forEach { Log.i("MyLog", "$it") }
+    }
+
+    fun testBreak() {
+        //xxx@  加标记，用来标记退出位置
+        label@ for (x in 1..100) {
+            label2@ for (y in 50..60) {
+                if (x == y) {
+                    Log.i("MyLog", "退出")
+                    break@label
+                }
+            }
+        }
+    }
+
+    fun testReturn() {
+        listOf(1, 2, 3, 4, 5).forEach {
+            if (it == 3) return // 非局部直接返回到 testReturn() 的调用者，整个方法后面的代码都不走了
+            Log.i("MyLog", "$it")
+        }
+        Log.i("MyLog", "this point is unreachable")
+
+        listOf(1, 2, 3, 4, 5).forEach lit@{
+            if (it == 3) return@lit // 局部返回到该 lambda 表达式的调用者，即 forEach 循环。会继续走循环
+            Log.i("MyLog", "$it")
+        }
+        Log.i("MyLog", "this point is unreachable")
+
+        listOf(1, 2, 3, 4, 5).forEach {
+            if (it == 3) return@forEach // 局部返回到该 lambda 表达式的调用者，即 forEach 循环。会继续走循环
+            Log.i("MyLog", "$it")
+        }
+        Log.i("MyLog", "this point is unreachable")
     }
 }
---
+++ app/src/main/java/com/spiderman/kotlinstudy/User.kt
@@ -0,0 +1,9 @@
+package com.spiderman.kotlinstudy
+
+/**
+ *Description：I won't write any description!
+ *Author ：Spider-Man.
+ *Date：2019/2/25
+ */
+class User {
+}
